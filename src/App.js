796fad04ae1fa54c260ac6838c90a00e6d6c869c
greenwhale00 <goreakay@gmail.com>
2022-11-01 11:49:50 +0900

    221101

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import List from './List';
 import './common.scss'
 
 function App() {
+
+  const genreList = [
+    "Action",
+    "Fantasy",
+    "Romance",
+    "Thriller",
+    "Comedy",
+    "Crime",
+    "Animation",
+    "Adventure",
+    "Drama",
+    "Western"
+  ]
+
+
   return (
-    <div className="App">
-      <List genre='Drama' limit={20} />
-      <List genre='Action' limit={20} />
-      <List genre='horror' limit={20} />
-    </div>
+    <div>
+
+      <ul className="List">
+        {
+          genreList.map(it => {
+return {
+  <li>
+  <Link to ={it}>{it}</Link>
+  </li>
+}
+          })
+        }
+      </ul>
+
+<Routes>
+  {
+    genreList.map(it => {
+      return (
+        <Route path={genreList[0]} />
+      )
+    })
+  }
+  
+</Routes>
+
+
+
+
+     
+        <List genre='Drama' limit={20} />
+        <List genre='Action' limit={20} />
+        <List genre='horror' limit={20} />
+     
+
+    </div >
+
   );
 }
 
